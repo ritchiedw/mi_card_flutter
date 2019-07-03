2ff02104275ac269f0e68bd6d8f8da6886830a7f
ritchied <david.ritchie@argyll-bute.gov.uk>
2019-07-03 16:35:06 +0100

    part way through

--- lib/main.dart
+++ lib/main.dart
@@ -11,14 +11,32 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Container(
-            height: 100.0,
-            width: 100.0,
-            margin: EdgeInsets.only(left: 30.0),
-            padding: EdgeInsets.all(20.0),
-            color: Colors.white,
-            child: Text("Hello"),
-          ),
+            child: Column(
+                //verticalDirection: VerticalDirection.down,
+                //mainAxisSize: MainAxisSize.min,
+                //mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: <Widget>[
+                  Container(
+                    height: 100.0,
+                    width: 100.0,
+                    color: Colors.white,
+                    child: Text("Container 1"),
+                  ),
+                  Container(
+                    width: 100.0,
+                    height: 100.0,
+                    color: Colors.blue,
+                    child: Text("Container 2"),
+                  ),
+                  Container(
+                    width: 100.0,
+                    height: 100.0,
+                    color: Colors.red,
+                    child: Text("Container 3"),
+                  ),
+                ]
+            )
         ),
       ),
     );
