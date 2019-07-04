b9a619a853be050c3e2077afb1bf98fba7d8da4e
ritchied <david.ritchie@argyll-bute.gov.uk>
2019-07-04 11:56:18 +0100

    some padding around row

--- lib/main.dart
+++ lib/main.dart
@@ -37,33 +37,34 @@ class MyApp extends StatelessWidget {
                   letterSpacing: 2.5,
                 )
               ),
-              Container(
+              Card(
                 color: Colors.white,
-                padding: EdgeInsets.all(10.0),
                 margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                child: Row(
-                  children: <Widget>[
-                    Icon(
-                        Icons.phone,
-                        color: Colors.teal,
-                    ),
-                    SizedBox(
-                      width: 10.0,
-                    ),
-                    Text(
-                      '12345678900',
-                      style: TextStyle(
-                        color: Colors.teal.shade900,
-                        fontFamily: 'SourceSansPro',
-                        fontSize: 20.0,
+                child: Padding(
+                  padding: EdgeInsets.all(25.00),
+                  child: Row(
+                    children: <Widget>[
+                      Icon(
+                          Icons.phone,
+                          color: Colors.teal,
+                      ),
+                      SizedBox(
+                        width: 10.0,
+                      ),
+                      Text(
+                        '12345678900',
+                        style: TextStyle(
+                          color: Colors.teal.shade900,
+                          fontFamily: 'SourceSansPro',
+                          fontSize: 20.0,
+                        )
                       )
-                    )
-                  ]
-        )
+                    ]
         ),
-              Container(
+                )
+        ),
+              Card(
                   color: Colors.white,
-                  padding: EdgeInsets.all(10.0),
                   margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
                   child: Row(
                       children: <Widget>[
