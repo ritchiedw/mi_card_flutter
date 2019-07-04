e74de774d54d3c03e8bb0796a59ac2fd6ffb7662
ritchied <david.ritchie@argyll-bute.gov.uk>
2019-07-04 10:16:54 +0100

    made the circle avatar have ma fizzer in it

--- lib/main.dart
+++ lib/main.dart
@@ -11,43 +11,15 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-            child: Row(
-                //verticalDirection: VerticalDirection.down,
-                //mainAxisSize: MainAxisSize.min,
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                //crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: <Widget>[
-                  Container(
-                    width: 100.0,
-                    height: double.infinity,
-                    color: Colors.red,
-                    //child: Text("Container 1"),
-                  ),
-                  Expanded(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Container(
-                          width: 100.0,
-                          height: 100.0,
-                          color: Colors.yellow,
-                        ),
-                        Container(
-                          width: 100.0,
-                          height: 100.0,
-                          color: Colors.green,
-                        ),
-                      ],
-                    )
-                  ),
-                  Container(
-                    width: 100.0,
-                    height: double.infinity,
-                    color: Colors.blue,
-                    //child: Text("Container 3"),
-                  ),
-                ]
-            )
+          child: Column(
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50.0,
+                backgroundColor: Colors.red,
+                backgroundImage: AssetImage("images/me.jpg"),
+              ),
+            ],
+          ),
         ),
       ),
     );
